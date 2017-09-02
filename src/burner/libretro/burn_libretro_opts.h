feec1c3b7a5f92e0da1f791abe4ebe38fe026013
barbudreadmon <barbudreadmon@gmail.com>
2017-09-02 11:07:20 +0200

    Some cleanup

--- src/burner/libretro/burn_libretro_opts.h
+++ src/burner/libretro/burn_libretro_opts.h
@@ -5,9 +5,5 @@
  #include "streams/file_stream_transforms.h"
 #endif
 
-extern char g_rom_dir[1024];
-extern char g_save_dir[1024];
-extern char g_system_dir[1024];
-
 extern unsigned int (__cdecl *BurnHighCol) (signed int r, signed int g, signed int b, signed int i);
 #endif
