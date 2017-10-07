93eacaf8ff405775412836e076a95de31c875fe3
barbudreadmon <barbudreadmon@gmail.com>
2017-10-07 09:45:21 +0200

    Handle missing inputs in Jackal

--- src/burner/libretro/libretro.cpp
+++ src/burner/libretro/libretro.cpp
@@ -3448,6 +3448,18 @@ INT32 GameInpSpecialOne(struct GameInp* pgi, INT32 nPlayer, char* szi, char *szn
 		}
 	}
 
+	// Fix part of issue #102 (Jackal)
+	if ((parentrom && strcmp(parentrom, "jackal") == 0) ||
+		(drvname && strcmp(drvname, "jackal") == 0)
+	) {
+		if (strcmp("Rotate Left", description) == 0) {
+			GameInp2RetroInp(pgi, nPlayer, GIT_SWITCH, 0, false, RETRO_DEVICE_ID_JOYPAD_L, 0, description);
+		}
+		if (strcmp("Rotate Right", description) == 0) {
+			GameInp2RetroInp(pgi, nPlayer, GIT_SWITCH, 0, false, RETRO_DEVICE_ID_JOYPAD_R, 0, description);
+		}
+	}
+
 	// Fix part of issue #102 (Last Survivor)
 	if ((parentrom && strcmp(parentrom, "lastsurv") == 0) ||
 		(drvname && strcmp(drvname, "lastsurv") == 0)
