f71f2720dcd48a35d89ef666b4994c09c7f815e6
twinaphex <libretro@gmail.com>
2018-01-04 08:21:37 +0100

    Update VFS/filestream code to latest code in libretro-common - hope this causes zero complications but we need to remain in sync regardless

--- src/burner/libretro/libretro-common/streams/file_stream_transforms.c
+++ src/burner/libretro/libretro-common/streams/file_stream_transforms.c
@@ -28,34 +28,31 @@
 
 RFILE* rfopen(const char *path, const char *mode)
 {
+   RFILE          *output  = NULL;
    unsigned int retro_mode = RETRO_VFS_FILE_ACCESS_READ;
-   bool position_to_end = false;
+   bool position_to_end    = false;
 
    if (strstr(mode, "w"))
-   {
-	   retro_mode = RETRO_VFS_FILE_ACCESS_WRITE;
-   }
+      retro_mode = RETRO_VFS_FILE_ACCESS_WRITE;
 
    if (strstr(mode, "a"))
    {
-	   retro_mode = RETRO_VFS_FILE_ACCESS_READ_WRITE | RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING;
-	   position_to_end = true;
+      retro_mode = RETRO_VFS_FILE_ACCESS_READ_WRITE | 
+         RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING;
+      position_to_end = true;
    }
 
    if (strstr(mode, "+"))
    {
-	   retro_mode = RETRO_VFS_FILE_ACCESS_READ_WRITE;
-	   if (strstr(mode, "r"))
-	   {
-		   retro_mode = retro_mode & RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING;
-	   }
+      retro_mode = RETRO_VFS_FILE_ACCESS_READ_WRITE;
+      if (strstr(mode, "r"))
+         retro_mode = retro_mode & RETRO_VFS_FILE_ACCESS_UPDATE_EXISTING;
    }
 
-   RFILE* output = filestream_open(path, retro_mode, RETRO_VFS_FILE_ACCESS_HINT_NONE);
-   if (output != NULL && position_to_end)
-   {
-	   filestream_seek(output, 0, RETRO_VFS_SEEK_POSITION_END);
-   }
+   output = filestream_open(path, retro_mode,
+         RETRO_VFS_FILE_ACCESS_HINT_NONE);
+   if (output && position_to_end)
+      filestream_seek(output, 0, RETRO_VFS_SEEK_POSITION_END);
 
    return output;
 }
@@ -90,9 +87,9 @@ int rfseek(RFILE* stream, long offset, int origin)
 }
 
 size_t rfread(void* buffer,
-   size_t elementSize, size_t elementCount, RFILE* stream)
+   size_t elem_size, size_t elem_count, RFILE* stream)
 {
-   return filestream_read(stream, buffer, elementSize*elementCount);
+   return filestream_read(stream, buffer, elem_size * elem_count);
 }
 
 char *rfgets(char *buffer, int maxCount, RFILE* stream)
@@ -106,9 +103,9 @@ int rfgetc(RFILE* stream)
 }
 
 size_t rfwrite(void const* buffer,
-   size_t elementSize, size_t elementCount, RFILE* stream)
+   size_t elem_size, size_t elem_count, RFILE* stream)
 {
-   return filestream_write(stream, buffer, elementSize*elementCount);
+   return filestream_write(stream, buffer, elem_size * elem_count);
 }
 
 int rfputc(int character, RFILE * stream)
@@ -128,10 +125,10 @@ int rfprintf(RFILE * stream, const char * format, ...)
 
 int rferror(RFILE* stream)
 {
-    return filestream_error(stream);
+   return filestream_error(stream);
 }
 
 int rfeof(RFILE* stream)
 {
-    return filestream_eof(stream);
+   return filestream_eof(stream);
 }
